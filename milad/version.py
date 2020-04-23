697b00ccc2f2ec2003d02c2d76aea868c6862a01
Martin Uhrin <martin.uhrin@gmail.com>
2020-04-23 11:20:31 +0200

    Release 0.1.0

--- milad/version.py
+++ milad/version.py
@@ -1,5 +1,5 @@
 author_info = (('Martin Uhrin', 'martin.uhrin.10@ucl.ac.uk'),)
-version_info = (0, 0, 1)
+version_info = (0, 1, 0)
 
 __author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
 __version__ = ".".join(map(str, version_info))
