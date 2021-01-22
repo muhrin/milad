Fixed bug in Jacobian of apply cutoff

milad/atomic.py
@@ -37,8 +37,9 @@ class AtomsCollection(functions.PlainState):
     def total_length(num_atoms: int) -> int:
         return AtomsCollection.num_atomic_properties() * num_atoms
 
-    def __init__(self, num: int, positions: np.array = None, numbers: np.array = None):
-        super().__init__(self.total_length(num))
+    def __init__(self, num: int, positions: np.array = None, numbers: np.array = None, dtype=None):
+        super().__init__(self.total_length(num), dtype=dtype)
+
         self._num_atoms = num
         if positions is not None:
             self.positions = positions
@@ -328,7 +329,7 @@ class ScalePositions(functions.Function):
         return len(in_state)
 
     def evaluate(self, atoms: AtomsCollection, get_jacobian=False):
-        out_atoms = AtomsCollection(atoms.num_atoms)
+        out_atoms = AtomsCollection(atoms.num_atoms, dtype=atoms.dtype)
         out_atoms.positions[:] = self._scale_factor * atoms.positions
         out_atoms.numbers[:] = atoms.numbers
 
@@ -478,7 +479,7 @@ class ApplyCutoff(functions.Function):
         if get_jacobian:
             jac = np.zeros((len(out_atoms), len(in_atoms)))
             for old_idx, new_idx in index_map.items():
-                jac[out_atoms.linear_pos_idx(new_idx), in_atoms.linear_pos_idx(old_idx)] = 1.
+                jac[out_atoms.linear_pos_idx(new_idx), in_atoms.linear_pos_idx(old_idx)] = np.eye(3)
                 jac[out_atoms.linear_number_idx(new_idx), in_atoms.linear_number_idx(old_idx)] = 1.
 
             return out_atoms, jac

milad/functions.py
@@ -45,8 +45,8 @@ StateLike = Union[np.ndarray, State]
 class PlainState(State):
     __slots__ = ('_array',)
 
-    def __init__(self, length):
-        self._array = np.zeros(length)
+    def __init__(self, length, dtype=None):
+        self._array = np.zeros(length, dtype=dtype)
 
     @property
     def vector(self) -> np.array:
@@ -60,6 +60,10 @@ class PlainState(State):
     @array.setter
     def array(self, arr: np.ndarray):
         self._array[:] = arr
+
+    @property
+    def dtype(self):
+        return self._array.dtype
 
 
 class Feature(State):

milad/models/neuralnetwork.py
@@ -450,7 +450,7 @@ class NeuralNetwork:
             sequence.append(self._activations)
             prev_size = size
         # Add the output layer
-        sequence.extend((torch.nn.Linear(prev_size, 1, bias=False), self._energy_scaler))
+        sequence.extend((torch.nn.Linear(prev_size, 1, bias=True), self._energy_scaler))
 
         self._network = torch.nn.Sequential(*sequence)
         self._network.to(self._device, self._dtype)

milad/optimisers.py
@@ -27,8 +27,10 @@ class LeastSquaresOptimiser:
         func: functions.Function,
         initial: functions.StateLike,
         jacobian='2-point',
+        bounds=(-np.inf, np.inf),
+        max_force_evals=None,
+        grad_tol=1e-6,
         verbose=False,
-        bounds=(-np.inf, np.inf)
     ):
         """
         :param func: the function to optimise
@@ -36,6 +38,9 @@ class LeastSquaresOptimiser:
         :param jacobian: if 'native' the analytic Jacobian will be requested from 'func', otherwise this option is
             passed to optimize.least_squares
         :param bounds: place bounds on the possible inputs to func
+        :param max_force_evals: the maximum number of force evaluations.  If not specified will be 50 * len(initial).
+        :param verbose: print during the optimisation
+
         :return:
         """
 
@@ -53,11 +58,21 @@ class LeastSquaresOptimiser:
         # but we don't support getting the Jacobian on it's own (it always comes
         # with a function evaluation and we don't want to call it twice so just
         # cache the result)
-        jac = self._jac if jacobian == 'native' else '2-point'
+        jac = self._jac if jacobian == 'native' else jacobian
         data = LeastSquaresOptimiser.Data(use_jacobian=jacobian == 'native', verbose=verbose)
 
+        max_force_evals = max_force_evals if max_force_evals is not None else 100 * len(initial)
+
         # Do it!
-        return optimize.least_squares(self._calc, x0_, jac=jac, kwargs=dict(func=fun, opt_data=data), bounds=bounds)
+        return optimize.least_squares(
+            self._calc,
+            x0_,
+            jac=jac,
+            kwargs=dict(func=fun, opt_data=data),
+            bounds=bounds,
+            gtol=grad_tol,
+            max_nfev=max_force_evals,
+        )
 
     def optimise_target(
         self,
@@ -65,8 +80,10 @@ class LeastSquaresOptimiser:
         initial: functions.StateLike,
         target: functions.StateLike,
         jacobian='2-point',
-        verbose=False,
         bounds=(-np.inf, np.inf),
+        max_force_evals=None,
+        grad_tol=1e-6,
+        verbose=False,
     ):
         """
         Optimise the function to a given target value.  This minimises the loss of the difference between the function
@@ -78,6 +95,8 @@ class LeastSquaresOptimiser:
         :param jacobian: if 'native' the analytic Jacobian will be requested from 'func', otherwise this option is
             passed to optimize.least_squares
         :param bounds: place bounds on the possible inputs to func
+        :param max_force_evals: the maximum number of force evaluations.  If not specified will be 50 * len(initial).
+        :param verbose: print during the optimisation
         :return:
         """
         # Calculate residuals to a particular target
@@ -86,7 +105,9 @@ class LeastSquaresOptimiser:
             initial=initial,
             jacobian=jacobian,
             verbose=verbose,
-            bounds=bounds
+            bounds=bounds,
+            grad_tol=grad_tol,
+            max_force_evals=max_force_evals
         )
 
     def _calc(

milad/zernike.py
@@ -139,11 +139,11 @@ class ZernikeMoments(base_moments.Moments):
         self._dtype = dtype
 
         self._moments = {}
-        self._moments[0] = np.empty((n_max + 1, n_max + 1), dtype=float)
+        self._moments[0] = np.empty((n_max + 1, n_max + 1), dtype=dtype)
         for m in inclusive(1, n_max):
             shape = (n_max - m + 1, n_max + 1)
-            self._moments[m] = np.empty(shape, dtype=complex)
-            self._moments[-m] = np.empty(shape, dtype=complex)
+            self._moments[m] = np.empty(shape, dtype=dtype)
+            self._moments[-m] = np.empty(shape, dtype=dtype)
 
     def __eq__(self, other: 'ZernikeMoments'):
         """Test if another set of moments are equal to this one"""
@@ -197,12 +197,15 @@ class ZernikeMoments(base_moments.Moments):
             n, l, m = key
 
         if m == 0:
-            if value.imag > 1e-9:
-                logging.warning(
-                    f'Trying to set value of moment {n},{l},{m} to {value}, '
-                    f'however these moment should always be real so discarding imaginary'
-                )
-            value = np.real(value)
+            try:
+                if value.imag > 1e-9:
+                    logging.warning(
+                        f'Trying to set value of moment {n},{l},{m} to {value}, '
+                        f'however these moment should always be real so discarding imaginary'
+                    )
+                value = np.real(value)
+            except AttributeError:
+                pass
 
         l_idx = l - abs(m)
         self._moments[m][l_idx, n] = value