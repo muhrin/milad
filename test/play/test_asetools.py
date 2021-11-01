7f08bd931bbd245328c595da57df1ad73e89877d
Martin Uhrin <martin.uhrin@gmail.com>
2021-11-01 10:50:38 +0100

    Removed legacy fingerprinting code

--- test/play/test_asetools.py
+++ test/play/test_asetools.py
@@ -4,61 +4,11 @@ import math
 import ase.build
 import ase.io
 import ase.neighborlist
-import matplotlib.pyplot as plt
 import numpy as np
 
-import milad
 from milad.play import asetools
 
 # pylint: disable=invalid-name
-
-
-def test_multiple_species(geometric_invariants, request, save_figures):
-    molecule = ase.build.molecule('CH3CH2OH')
-    species = ['C', 'H', 'O']
-    symbols = molecule.symbols
-    num_atoms = len(symbols)
-
-    fp = asetools.MiladFingerprint(species, geometric_invariants, sigmas={'C': 1., 'H': 0.5, 'O': 0.7})
-
-    fingerprints = fp.create(molecule)
-
-    labels = []
-    for i in range(num_atoms):
-        labels.append('{}$_{}$'.format(i, symbols[i]))
-
-    assert (fingerprints[3] - fp.create_single(molecule, 3)).max() == 0
-
-    fig, axes = plt.subplots()
-    axes.set_yscale('log')
-    milad.plot.plot_multiple_invariants(fingerprints, axes, labels=labels)
-    axes.set_title('C$_2$H$_6$O')
-    fig.legend()
-    if save_figures:
-        fig.savefig('{}.pdf'.format(request.node.name))
-
-
-def test_multiple_species_split(geometric_invariants, request, save_figures):
-    molecule = ase.build.molecule('CH3CH2OH')
-    species = ['C', 'H', 'O']
-
-    fp = asetools.MiladFingerprint(
-        species, geometric_invariants, sigmas={
-            'C': 0.4,
-            'H': 0.3,
-            'O': 0.5
-        }, split_specie_pairs=True, cutoffs=100.
-    )
-
-    fingerprint = fp.create_single(molecule, 2)
-
-    fig, axes = plt.subplots()
-    axes.set_yscale('log')
-    axes.set_title(str(molecule))
-    milad.plot.plot_multiple_invariants(fingerprint, axes, labels=species)
-    fig.legend()
-    if save_figures:
-        fig.savefig('{}.pdf'.format(request.node.name))
 
 
 def test_generate_environments_molecule():
